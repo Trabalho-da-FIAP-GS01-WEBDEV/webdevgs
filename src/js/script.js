aabe900978b4ff547640f1a0436745f8c77af039
Heitor Paganotto <paganottoheitor079@gmail.com>
2025-06-06 01:29:33 -0300

    Codigo JS do Slide Show

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,15 @@ function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('show');
 }
+
+// SlideShow
+let currentSlide = 0;
+const slides = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg'];
+
+function showSlide() {
+    const slide = document.getElementById('slide-show');
+    slide.src = slides[currentSlide];
+    currentSlide = (currentSlide + 1) % slides.length;
+}
+
+setInterval(showSlide, 5000); // troca a imagem a cada 5 seguhndos
